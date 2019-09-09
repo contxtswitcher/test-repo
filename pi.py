08ad0721fe5a76ed373c58712cc9b44b5664a681
contxtswitcher <contxtswitcher@gmail.com>
2019-09-09 14:59:41 +0430

    removed some code

--- pi.py
+++ pi.py
@@ -7,10 +7,6 @@ import random
 
 import requests
 
-mylist = [2, 109, False, 10, "Lorem", 482, "Ipsum"]
-
-rnd = random.choice(mylist)
-
 
 r = requests.get("https://google.com")
 print(r.status_code)
